706c643809e4153a06de1c9f84b92d44bbccfc19
Varvara <varyagagarina@gmail.com>
2022-12-26 15:11:32 +0300

    refactor: delete the commented code

--- src/constants/constants.ts
+++
@@ -1,1 +0,0 @@
-export const rootProducts = document.querySelector('.store__items') as HTMLElement;
--- src/pages/store-page/store.ts
+++ src/pages/store-page/store.ts
@@ -1,6 +1,4 @@
 import Page from '../../components/page';
-//import CATALOG from '../../constants/catalog';
-//import { rootProducts } from '../../constants/constants';
 
 class StorePage extends Page {
   static CodeBlock = {
@@ -19,33 +17,6 @@ class StorePage extends Page {
     this.container.append(title);
     return this.container;
   }
-
-  // renderItems() {
-  //   const block = document.createElement('div');
-  //   block.className = 'store__products';
-  //   let htmlCatalog = '';
-
-  //   CATALOG.forEach(({ name, price, img }) => {
-  //     htmlCatalog += `
-  //             <li class="item">
-  //                 <img src="${img}" />
-  //                 <span class="item__name">${name}</span>
-  //                 <span class="item__price">${price}</span>
-  //                 <button class="item__button">Добавить в корзину</button>
-  //             </li>
-  //         `;
-  //   });
-
-  //   const html = `
-  //         <ul class="store__list">
-  //             ${htmlCatalog}
-  //         </ul>
-  //     `;
-
-  //   block.innerHTML = html;
-
-  //   return block;
-  // }
 }
 
 export default StorePage;
