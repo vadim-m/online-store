5efce72ee60805233515ae031b969e3e57be6639
Varvara <varyagagarina@gmail.com>
2023-01-05 02:47:33 +0300

    feat: add modal form

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -7,6 +7,7 @@ import Header from '../header/header';
 import Footer from '../footer/footer';
 import ErrorPage from '../../pages/error-page/error';
 import { PageIds } from '../../types/types';
+import { addEvents } from '../../helpers/utils';
 
 class App {
   private static container: HTMLElement = document.querySelector('.wrapper') as HTMLElement;
@@ -51,6 +52,11 @@ class App {
 
       pageHTML.id = App.defaultPageId;
       App.container.insertBefore(pageHTML, before);
+
+      // MODAL FORM - перенести
+      const buttonBuy = document.getElementById('buy');
+      if (buttonBuy) addEvents();
+      // КОНЕЦ
     }
   }
 
--- src/types/constants.ts
+++ src/types/constants.ts
@@ -11,3 +11,19 @@ export const PageLinks = [
     id: PageIds.ProductPage,
   },
 ];
+
+export const EMAIL_REGEXP =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+export const NAME_REGEXP = /^[A-Za-zА-Яа-яЁё]{3,}\s{1,}([A-Za-zА-Яа-яЁё]{3,}){1,}$/gi;
+
+export const PHONE_REGEXP = /\+\d{9,}$/gi;
+
+export const ADDRESS_REGEXP =
+  /^[A-Za-zА-Яа-яЁё]{5,}\s{1,}[A-Za-zА-Яа-яЁё]{5,}\s{1,}([A-Za-zА-Яа-яЁё]{5,}){1,}$/gi;
+
+export const CARD_REGEXP = /\d{4}\s\d{4}\s\d{4}\s\d{4}/g;
+
+export const DATE_REGEXP = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/gi;
+
+export const CVV_REGEXP = /\d{3}/g;
