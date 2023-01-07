1d5265404d13edb0d270237999f5e967d3048da6
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-01-07 19:52:09 +0300

    feat: add render filters options

--- src/components/catalog/catalogFilters.ts
+++ src/components/catalog/catalogFilters.ts
@@ -1,113 +1,41 @@
 import Component from '../component';
+import FilterCheckbox from '../filter/filterCheckbox';
 
 class CatalogFilters extends Component {
+  public filterBrand: FilterCheckbox;
+  public filterCategory: FilterCheckbox;
+  public filterColor: FilterCheckbox;
+
   constructor(tagName: string, className: string) {
     super(tagName, className);
+    this.filterBrand = new FilterCheckbox('fieldset', 'filters__group', 'Бренд:', 'brand');
+    this.filterCategory = new FilterCheckbox(
+      'fieldset',
+      'filters__group',
+      'Категории:',
+      'category'
+    );
+    this.filterColor = new FilterCheckbox('fieldset', 'filters__group', 'Цвет:', 'color');
+  }
+
+  renderFilters() {
+    const container = document.createElement('form');
+    container.className = 'filters';
+
+    container.append(this.filterBrand.renderCheckbox());
+    container.append(this.filterCategory.renderCheckbox());
+    container.append(this.filterColor.renderCheckbox());
+
+    return container;
   }
 
   render() {
-    this.container.innerHTML = `
-      <form class="filters">
-        <fieldset class="filters__group">
-          <legend class="filters__subtitle">Бренд:</legend>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            DomaYA
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Kucha
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            IKEA
-          </label>
-        </fieldset>
+    this.container = this.renderFilters();
 
-        <fieldset class="filters__group">
-          <legend class="filters__subtitle">Категория:</legend>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Бокалы
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Кружки
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Приборы
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Сервизы
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Стаканы
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Тарелки
-          </label>
-        </fieldset>
-
-        <fieldset class="filters__group">
-          <legend class="filters__subtitle">Цвет:</legend>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Белый
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Золотой
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Зеленый
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Прозрачный
-          </label>
-          <label class="filters__checkbox">
-            <input class="filters__input hide" type="checkbox">
-            <span class="filters__checkbox-span"></span>
-            Черный
-          </label>
-        </fieldset>
-
-        <fieldset class="filters__group">
-          <legend class="filters__subtitle">Цена:</legend>
-          <input calss="filters__range" type="range">
-        </fieldset>
-
-        <fieldset class="filters__group">
-          <legend class="filters__subtitle">Наличие:</legend>
-          <input calss="filters__range" type="range">
-        </fieldset>
-
-        <fieldset class="filters__buttons">
-          <button class="filters__btn">Сбросить</button>
-          <button class="filters__btn">Копировать</button>
-        </fieldset>
-    </form>
-   `;
     return this.container;
   }
+
+  // addEventList() {}
 }
 
 export default CatalogFilters;
---
+++ src/components/filter/filterCheckbox.ts
@@ -0,0 +1,48 @@
+import Component from '../component';
+import PRODUCTS from '../../data/products';
+
+class FilterCheckbox extends Component {
+  private filterName = '';
+  private category = '';
+
+  constructor(tagName: string, className: string, filterName: string, category: string) {
+    super(tagName, className);
+    this.filterName = filterName;
+    this.category = category;
+  }
+
+  addCheckbox(productKey: string) {
+    const filterOptions = Array.from(
+      new Set(PRODUCTS.map((product) => product[productKey]))
+    ).sort();
+    console.log(filterOptions);
+
+    let htmlTemplate = '';
+
+    filterOptions.forEach((option) => {
+      htmlTemplate += `
+        <label class="filters__checkbox">
+          <input class="filters__input hide" type="checkbox">
+          <span class="filters__checkbox-span"></span>
+          ${option}
+        </label>
+      `;
+    });
+
+    return htmlTemplate;
+  }
+
+  renderCheckbox() {
+    const labels = this.addCheckbox(this.category);
+    const htmlTemplate = `
+      <legend class="filters__subtitle">${this.filterName}</legend>
+        ${labels}
+      </label>
+    `;
+    this.container.innerHTML = htmlTemplate;
+
+    return this.container;
+  }
+}
+
+export default FilterCheckbox;
