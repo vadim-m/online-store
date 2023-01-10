99c1a76b2e16bf239a62c1fa7e930e85f03c3d68
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-01-10 20:20:22 +0300

    feat: add productFull render product__thumbnails

--- src/components/products/productFull.ts
+++ src/components/products/productFull.ts
@@ -40,30 +40,28 @@ class ProductFull extends Component {
               </div>
               <ul class="product__thumbnails">
                 <li class="product__thumbnail active">
-                  <button class="product__thumbnail-pic"></button>
+                  <button class="product__thumbnail-pic"style="background-image: url(${item.images[0]})">
+                  </button>
                 </li>
                 <li class="product__thumbnail">
-                  <button class="product__thumbnail-pic"></button>
+                  <button class="product__thumbnail-pic" style="background-image: url(${item.images[1]})"></button>
                 </li>
                 <li class="product__thumbnail">
-                  <button class="product__thumbnail-pic"></button>
+                  <button class="product__thumbnail-pic" style="background-image: url(${item.images[2]})"></button>
                 </li>
               </ul>
             </div>
             <div class="product__content">
               <div class="product__info">
                 <h3 class="product__name">${item.title}</h3>
-                <div class="product__price active">${item.price} ₽
-                  <span class="product__discount-price">50000 ₽</span>
-                </div>
+                <div class="product__price">${item.price} ₽</div>
+              </div>
+              <div class="product__brand">Бренд:
+                <span class="product__brand-value">${item.brand}</span>
               </div>
               <div class="product__full-info">
                 <h4 class="product__description">Описание товара:</h4>
-                <p class="product__text">${item.description}
-                </p>
-                <div class="product__stock">В наличии:
-                  <span class="product__stock-value">${item.stock}</span>
-                </div>
+                <p class="product__text">${item.description}</p>
                 <div class="product__category">Категория:
                   <span class="product__category-value">${item.category}</span>
                 </div>
@@ -73,6 +71,9 @@ class ProductFull extends Component {
                 <div class="product__material">Материал:
                   <span class="product__material-value">${item.material}</span>
                 </div>
+                <div class="product__stock">В наличии:
+                <span class="product__stock-value">${item.stock}</span>
+              </div>  
               </div>
               <div class="product__buttons">
                 <div class="product__count">
